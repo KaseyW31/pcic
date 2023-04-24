6969fdeb5f16d5b92012799d1cbd453980a21eda
Kasey Wei <kxw497@case.edu>
2023-04-24 14:55:11 -0400

    Documentation for Application

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,9 @@
 import java.util.logging.*;
 
+/**
+ * This class is an abstract representation of an Application that can be connected to a Device. To extend the class,
+ * the user only needs to provide an implementation for the receive method, depending on their needs.
+ */
 public abstract class Application {
 
     private static final Logger logger = Logger.getLogger(Application.class.getName());
@@ -8,16 +12,30 @@ public abstract class Application {
 
     protected Application() {}
 
+    /**
+     * Returns the device that the application is connected to. In order to be connected to a device, a Device object
+     * must be constructed with a map containing a mapping of a port ID to this application.
+     * @return the device that this application is connected to
+     */
     public Device getDevice() {
         return device;
     }
 
-    /* Invoked by the device when plugging the application in */
+    /**
+     * Invoked by the Device's static constructor when mapping port IDs to its connected applications.
+     * @param d the Device that calls this method, which this application will be connected to
+     */
     protected void setDevice(Device d) {
         device = d;
     }
 
-    /* Starting point of sending a message to another application */
+    /**
+     * Initiates the message sending process from this application to another. The message will be passed onto the
+     * application's connected device (if present) to be delegated to the motherboard.
+     * @param recID the ID of the recipient device, as mapped on the motherboard
+     * @param portID the ID of the port application, as mapped on the receiving device
+     * @param payload the message string to be sent
+     */
     public void send(int recID, int portID, String payload) {
         if (getDevice() == null) {
             logger.log(Level.WARNING, "Message could not be sent. Application is not connected to a device");
@@ -29,5 +47,9 @@ public abstract class Application {
         getDevice().delegateSend(message);
     }
 
+    /**
+     * Processes a message sent to this application.
+     * @param payload The message that was sent
+     */
     abstract void receive(String payload);
 }
